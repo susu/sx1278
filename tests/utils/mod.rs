564f42c11e514aafe08d80e605300367d37373c9
Marton Suranyi <marton.suranyi@gmail.com>
2018-04-05 22:19:06 +0200

    add registry_eq macro for better binary output

--- tests/utils/mod.rs
+++ tests/utils/mod.rs
@@ -16,6 +16,22 @@ use self::mockpin::MockPin;
 
 use sx1278::{SX1278, LoRa};
 
+#[macro_export]
+macro_rules! registry_eq {
+    ($left:expr, $right:expr) => ({
+        match (&$left, &$right) {
+            (left_val, right_val) => {
+                if !(*left_val == *right_val) {
+                    panic!(r#"assertion failed: `(left == right)`
+  left: `{:#010b}`,
+ right: `{:#010b}`"#, left_val, right_val)
+                }
+            }
+        }
+    });
+}
+
+
 pub fn create_sx1278() -> (SX1278<MockSpi, MockPin, LoRa>, Rc<RefCell<MockDevice>>) {
     let device = Rc::new(RefCell::new(MockDevice::new()));
     let sx = SX1278::new_lora(MockSpi::new(Rc::clone(&device)), MockPin {set:false}).unwrap();
